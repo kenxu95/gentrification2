b8ea9dd96b958a0b1a67a190e45ad105dd78c568
sashkarimi <skarimi@stanford.edu>
2016-12-10 14:09:08 -0800

    Update textureExtractor.py

--- feature_extraction/textureExtractor.py
+++ feature_extraction/textureExtractor.py
@@ -12,6 +12,6 @@ class textureExtractor():
         features = cv2.bilateralFilter(img, 9, 75, 75)
             
         # Choose different thresholding techniques
-        features= cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+        features = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
         # _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return features
